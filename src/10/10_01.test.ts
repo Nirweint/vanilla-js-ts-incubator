d3be0285bda2614ac9fa7932130da242e77a272f
Nirweint <nirweint_1@mail.ru>
2021-11-11 12:57:16 +0300

    10 immutability finish

--- src/10/10_01.test.ts
+++ src/10/10_01.test.ts
@@ -1,4 +1,12 @@
-import {increaseAgeNew, moveToAnotherAddress, UserType, UserWithLaptopType} from "./10_01";
+import {
+    addNewCompany,
+    increaseAgeNew,
+    moveToAnotherAddress, moveUSerToOtherHouse, upgradeUserBooks,
+    upgradeUserLaptop,
+    UserType,
+    UserWithBooksType,
+    UserWithLaptopType, WithCompaniesType
+} from "./10_01";
 
 
 
@@ -11,7 +19,7 @@ test('reference type test', () => {
         },
     }
 
-    const olderUser = increaseAgeNew(user)
+    const olderUser =    increaseAgeNew(user)
 
     expect(olderUser.age).toBe(33)
     expect(user.age).toBe(32)
@@ -39,4 +47,107 @@ test('change address', () => {
     expect(changedAddressUser.address.city).toBe('Kiev')
     expect(user.laptop).toBe(changedAddressUser.laptop)
 
+})
+
+test('upgrade laptop to macbook', () => {
+    let user: UserWithLaptopType = {
+        name: 'Dimych',
+        age: 32,
+        address: {
+            city: 'Minsk',
+            house: 12,
+        },
+        laptop: {
+            title: 'Zenbook',
+        }
+    }
+
+    const userWithUpgradedLaptop = upgradeUserLaptop(user, "Macbook")
+
+    expect(user).not.toBe(userWithUpgradedLaptop)
+    expect(user.laptop).not.toBe(userWithUpgradedLaptop.laptop)
+    expect(user.laptop.title).toBe('Zenbook')
+    expect(userWithUpgradedLaptop.laptop.title).toBe('Macbook')
+
+})
+
+test('move user to another house', () => {
+    let user: UserWithLaptopType & UserWithBooksType = {
+        name: 'Dimych',
+        age: 32,
+        address: {
+            city: 'Minsk',
+            house: 12,
+        },
+        laptop: {
+            title: 'Zenbook',
+        },
+        books: ['css', 'html', 'js', 'react'],
+    }
+
+    const userWithUpgradedHouse = moveUSerToOtherHouse(user, 99)
+
+    expect(user).not.toBe(userWithUpgradedHouse)
+    expect(user.laptop).toBe(userWithUpgradedHouse.laptop)
+    expect(user.laptop.title).toBe('Zenbook')
+    expect(user.books).toBe(userWithUpgradedHouse.books)
+    expect(userWithUpgradedHouse.address.house).toBe(99)
+
+})
+
+test('upgrade books', () => {
+    let user: UserWithLaptopType & UserWithBooksType = {
+        name: 'Dimych',
+        age: 32,
+        address: {
+            city: 'Minsk',
+            house: 12,
+        },
+        laptop: {
+            title: 'Zenbook',
+        },
+        books: ['css', 'html', 'js', 'react'],
+    }
+
+    const userWithUpgradedBooks = upgradeUserBooks(user, ['ts', 'rest api'])
+
+    expect(user).not.toBe(userWithUpgradedBooks)
+    expect(user.laptop.title).toBe('Zenbook')
+    expect(user.laptop).toBe(userWithUpgradedBooks.laptop)
+    expect(user.address).toBe(userWithUpgradedBooks.address)
+    expect(user.books).not.toBe(userWithUpgradedBooks.books)
+    expect(userWithUpgradedBooks.books).toStrictEqual(['css', 'html', 'js', 'react', 'ts', 'rest api'])
+
+})
+
+
+test('upgrade companies', () => {
+    let user: UserWithLaptopType & UserWithBooksType & WithCompaniesType = {
+        name: 'Dimych',
+        age: 32,
+        address: {
+            city: 'Minsk',
+            house: 12,
+        },
+        laptop: {
+            title: 'Zenbook',
+        },
+        books: ['css', 'html', 'js', 'react'],
+        companies: [
+            {title: 'EPAM', id: 1},
+            {title: 'it-incubator', id: 2},
+            {title: 'ITechArt-Group', id: 3},
+        ]
+    }
+
+    const userWithUpgradedCompanies = addNewCompany(user, "Wargaming")
+
+    expect(user).not.toBe(userWithUpgradedCompanies)
+    expect(user.laptop.title).toBe('Zenbook')
+    expect(user.laptop).toBe(userWithUpgradedCompanies.laptop)
+    expect(user.address).toBe(userWithUpgradedCompanies.address)
+    expect(user.books).toBe(userWithUpgradedCompanies.books)
+    expect(user.companies).not.toBe(userWithUpgradedCompanies.companies)
+    expect(userWithUpgradedCompanies.books).toStrictEqual(['css', 'html', 'js', 'react'])
+    expect(userWithUpgradedCompanies.companies[3].title).toBe("Wargaming")
 })
--- src/10/10_01.ts
+++ src/10/10_01.ts
@@ -12,6 +12,14 @@ export type UserWithLaptopType = UserType & {
     laptop: LaptopType
 }
 
+export type UserWithBooksType = UserType & {
+    books: string[]
+}
+
+export type WithCompaniesType = {
+    companies: Array<{ title: string, id: number}>
+}
+
 export function increaseAgeNew(u: UserType) {
     const copy = {
         ...u, age: u.age + 1
@@ -22,5 +30,21 @@ export function increaseAgeNew(u: UserType) {
 }
 
 export const moveToAnotherAddress = (u: UserWithLaptopType, city: string) => {
-    return {...u, address : {...u.address, city}}
+    return {...u, address : {...u.address, city: city}}
+}
+
+export const moveUSerToOtherHouse = (u: UserWithLaptopType & UserWithBooksType, house: number) => {
+    return {...u, address : {...u.address, house: house}}
+}
+
+export const upgradeUserLaptop = (u: UserWithLaptopType, title: string) => {
+    return {...u, laptop: {...u.laptop, title: title}}
+}
+
+export const upgradeUserBooks = (u: UserWithLaptopType & UserWithBooksType, newBooks: string[]) => {
+    return {...u, books: [...u.books, ...newBooks]}
+}
+
+export const addNewCompany = (u: UserWithLaptopType & UserWithBooksType & WithCompaniesType, newCompany: string) => {
+    return {...u, companies: [...u.companies, {title: newCompany, id: 4}]}
 }
